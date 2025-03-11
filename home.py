8a7ea1dc6103e6f9eabc76bff76a7c6f7b05b9d9
Dirga Halim Susilo <86204615+8shagrid@users.noreply.github.com>
2025-03-11 13:50:04 +0700

    Add files via upload

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ def show_home():
     col1, col2 = st.columns(2)
     with col1:
         # Gambar visual atau elemen grafik yang menarik
-        st.image("images/pexels-anna-nekrashevich-6801648.jpg", use_column_width=True)
+        st.image("images/pexels-anna-nekrashevich-6801648.jpg", use_container_width=True)
 
     with col2:
         st.write("")
